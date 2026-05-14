46eb1105192cbf8b47249f158716c991f7b100db
Melvin Kamguia <kamguiamelvin@gmail.com>
2026-05-14 04:41:28 -0400

    modification

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,52 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const axios = require("axios");
+
+public_users.get("/author/:author", async function (req, res) {
+    const author = req.params.author;
+
+    try {
+        const books = await new Promise((resolve) => {
+            resolve(require("./booksdb.js"));
+        });
+
+        let filteredBooks = {};
+
+        for (let isbn in books) {
+            if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+                filteredBooks[isbn] = books[isbn];
+            }
+        }
+
+        if (Object.keys(filteredBooks).length > 0) {
+            return res.status(200).json(filteredBooks);
+        } else {
+            return res.status(404).json({ message: "No books found for this author" });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching books by author" });
+    }
+});
+public_users.get("/isbn/:isbn", async function (req, res) {
+    const isbn = req.params.isbn;
+
+    try {
+        const books = await new Promise((resolve) => {
+            resolve(require("./booksdb.js"));
+        });
+
+        if (books[isbn]) {
+            return res.status(200).json(books[isbn]);
+        } else {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching book details" });
+    }
+});
 
 public_users.post("/register", (req, res) => {
 
